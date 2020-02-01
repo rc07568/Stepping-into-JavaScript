a64f72360234c8fe9e8ca645956af80354473227
rcarver <rc07568@ravens.racc.edu>
2020-02-01 13:41:42 -0500

    updated the test.js file with an if else statement that document.write if you are over a certain age

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
-var youLikMeat = true;
+let myAge = 44;
 
-if (youLikMeat){
+if (myAge > 40){
 	
-	document.write("You like meat!!");
+	document.write('you are over 40!');
 	
+}else if (myAge > 20){
+	
+	document.write('you are over 20!');
+	
+}else if (myAge > 10){
+	
+	document.write('you are over 10!');
 }else{
 	
-	document.write("You hate meat!!")
+	document.write('you are not over 10!');
 }
+	
+	
+
 
